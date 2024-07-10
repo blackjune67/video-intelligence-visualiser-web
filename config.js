287169fb78a0b016e573700c5bff76e4a936855c
blackjune67 <buea486@gmail.com>
2024-07-10 15:21:59 +0900

    test

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    GCP_SERVER_IP: '34.64.43.132',
+    GCP_SERVER_PORT: 5580,
+    LOCAL_HOST_PORT: 8080
+};
+
+// CommonJS module syntax
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
+// ES6 module syntax
+else if (typeof exports !== 'undefined') {
+    exports.default = CONFIG;
+}
+// Browser global
+else {
+    window.CONFIG = CONFIG;
+}
